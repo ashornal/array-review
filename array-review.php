747ae8c5c648df1782a6ddc0febd984c02eff96d
Alex Shornal <shornality@yahoo.com>
2018-01-17 01:12:41 -0500

    add animals function added

--- array-review.php
+++ array-review.php
@@ -17,4 +17,19 @@ function printAnimals($animals)
         echo $animals[$i] . " ";
     }
 }
+
+
+function addAnimals($string, $animals)
+{
+    echo nl2br ("\n adding $string ... \n");
+    $string = strtolower($string);
+    if(!(in_array($string, $animals)))
+    {
+        array_push($animals, $string);
+    }
+    printAnimals($animals);
+}
+
 printAnimals($animals);
+addAnimals("goat", $animals);
+addAnimals("Boa", $animals);
