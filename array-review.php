96a14b88d2c350bff250377681ca82a48d8f8861
Alex Shornal <shornality@yahoo.com>
2018-01-17 01:02:24 -0500

    defined array

---
+++ array-review.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 1/16/2018
+ * Time: 9:59 PM
+ */
+
+$animals = array("panda", "alpaca", "boa");
+
+function printAnimals($animals)
+{
+
+}
